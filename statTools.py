e959ceee3c083a99b38c5ed68656871c7047d59b
Ryan2401 <ryan.kwong19@ycdsbk12.ca>
2018-11-07 22:03:38 -0500

    Pass 3 tests for mean

--- statTools.py
+++ statTools.py
@@ -1,9 +1,18 @@
-'''
+
 def mean(mylist):
     count = 0
     sum = 0
     for elem in mylist:
         sum += elem
         count += 1
-    return sum/count
+    return round(sum/count,2)
+'''
+def range(mylist):
+    min_value = None
+    for value in mylist:
+        if not min_value:
+            min_value = value
+        elif value < min_value:
+            min_value = value
+    return min_value
 '''
--- test_statTools.py
+++ test_statTools.py
@@ -1,6 +1,9 @@
 from statTools import*
 import pytest
-'''
+
 def test_mean1():
     assert (mean([10,10,10,10,10,10,10,10,10,10])==10)
-'''
+def test_mean2():
+    assert (mean([12,432,453,122,324,32,-324,-746,21,-233])==9.3)
+def test_mean3():
+    assert (mean([4365,-43,-793,4354,31,213,-4233])==556.29)
