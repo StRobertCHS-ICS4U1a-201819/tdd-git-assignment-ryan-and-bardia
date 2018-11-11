6e70b1345ce03c23fc571061a18514edf60bb83f
BardiaG <44610967+BardiaG@users.noreply.github.com>
2018-11-11 10:23:30 -0500

    Update statTools.py

--- statTools.py
+++ statTools.py
@@ -1,2 +1,14 @@
-def mean(mylist):
-    count = 0
+def median(lst):
+  lst.sort()
+  a = (len(lst) / 2)
+  return lst[a]
+
+def upperQuart(lst):
+  lst.sort()   
+  a = ((3 * len(lst)) / 4)
+  return lst[a]
+
+def lowerQuart(lst):
+  lst.sort()
+  a = (len(lst) / 4)
+  return lst[a]
