9ac21ba70650f4ac3d1e101ae4d57c2404fa3b1a
Ryan2401 <ryan.kwong19@ycdsbk12.ca>
2018-11-10 21:01:52 -0500

    Pass 3 tests for Variance

--- statTools.py
+++ statTools.py
@@ -7,7 +7,21 @@ def mean(mylist):
         count += 1
     return round(sum/count,2)
 '''
+'''
 def range(mylist):
     least = min(mylist)
     greatest = max(mylist)
     return (greatest - least)
+'''
+
+def variance(mylist):
+    count = 0
+    firstsum = 0
+    secondsum = 0
+    for elem in mylist:
+        firstsum += elem
+        count += 1
+    mean = firstsum/count
+    for elem in mylist:
+        secondsum += (mean-elem)**2
+    return round(secondsum/count,2)
--- test_statTools.py
+++ test_statTools.py
@@ -8,9 +8,18 @@ def test_mean2():
 def test_mean3():
     assert (mean([4365,-43,-793,4354,31,213,-4233])==556.29)
 '''
+'''
 def test_range1():
     assert (range([1,2,3,4,5,6,7,8,9,19])==18)
 def test_range2():
     assert (range([324,532,-32,-432,23,12,2341,34,2,4])==2773)
 def test_range3():
     assert (range([23,12,312,12,0,23,12])==312)
+'''
+
+def test_variance1():
+    assert (variance([6,2,3,1])==3.5)
+def test_variance2():
+    assert (variance([32,12,1,23,2,9,24,6])==113.73)
+def test_variance3():
+    assert (variance([-23,7,45,21,18,-12,-4])==448.82)
