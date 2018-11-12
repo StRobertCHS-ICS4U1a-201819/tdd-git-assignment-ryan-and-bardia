22982e3647ea27eda64d50726a563798c4be3e70
BardiaG <bardia.gharachorlou19@ycdsbk12.ca>
2018-11-11 21:05:33 -0500

    Test Passed for mode()

--- statTools.py
+++ statTools.py
@@ -1,19 +1,30 @@
 def median(lst):
-  lst.sort()
-  a = int((len(lst) / 2))
-  return lst[a]
-'''
+    if len(lst) == 0:
+      return "Error: Empty List."
+    else:
+      lst.sort()
+      a = int((len(lst) / 2))
+      return lst[a]
 
 def mode(lst):
-  return max(set(lst), key=lst.count)
+  if len(lst) == 0:
+    return "Error: Empty List."
+  else:
+    return max(set(lst), key=lst.count)
 
 def upperQuart(lst):
-  lst.sort()   
-  a = ((3 * len(lst)) / 4)
-  return lst[a]
+  lst.sort()
+  if len(lst) == 0:
+    return "Error: Empty List."
+  else:
+    a = int((3 * len(lst) - 1) / 4)
+    return lst[a]
 
 def lowerQuart(lst):
   lst.sort()
-  a = (len(lst) / 4)
-  return lst[a]
-  '''
+  if len(lst) == 0:
+    return "Error: Empty List."
+  else:
+    a = int(len(lst) / 4)
+    return lst[a]
+
--- test_statTools.py
+++ test_statTools.py
@@ -1,12 +1,29 @@
 from statTools import*
 import pytest
 def test_median1():
-  assert (median([10,10,10,10,10,10,10,10,10,10]) == 10)
+  assert (median([100, 9, 3, 5, 4, 8, 16, 2, 6]) == 6)
 def test_median2():
-  assert (median([100, 9, 3, 6, 5, 4, 8, 16, 2]) == 6)
+  assert (median([-3, 4, 7, 13, 17, -4, -18, 10]) == 7)
 def test_median3():
-  assert (median([2, 3, 4, 5, 7, 7, 7, 16, 9, 100]) == 7)
+  assert (median([]) == "Error: Empty List.")
 
-test_median1()
-test_median2()
-test_median3()
+def test_mode1():
+  assert (mode([4, 1, 14, 5, 7, 43, 5, 11, 22, 1, 5, 58]) == 5)
+def test_mode2():
+  assert (mode([6, -1, 5, 11, 1, -24, 5, 5, -6, 78, 18, 90]) == 5)
+def test_mode3():
+  assert (mode([]) == "Error: Empty List.")
+
+def test_upperQuart1():
+  assert (upperQuart([1,3, 4, 5]) == 4)
+def test_upperQuart2():
+  assert (upperQuart([1, 4, 2, 8, 6, 17, 11, 44, 25, 18]) == 18)
+def test_upperQuart3():
+  assert (upperQuart([]) == "Error: Empty List.")
+
+def test_lowerQuart1():
+  assert (lowerQuart([1, 3, 4, 5]) == 3)
+def test_lowerQuart2():
+  assert (lowerQuart([1, 3, 4, 8, 10, 22, 27, 33, 34, 45, 56]) == 4)
+def test_lowerQuart2():
+  assert (lowerQuart([5, 6, 8, 10, 22, 33, 34, 45, 56]) == 6)
